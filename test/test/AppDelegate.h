e20960c4543766546dcbd3fc70363bc3a7bac740
肖朝阳 <534600451@qq.com>
2018-11-11 22:00:57 +0800

    11

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 肖朝阳 on 2018/8/21.
+//  Copyright © 2018年 肖朝阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 肖朝阳 on 2018/8/21.
+//  Copyright © 2018年 肖朝阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
