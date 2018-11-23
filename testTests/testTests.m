b1792c51413b750052840b9fbb406ebcbbdc0f15
肖朝阳 <534600451@qq.com>
2018-11-23 16:51:49 +0800

    1

--- testTests/testTests.m
+++ testTests/testTests.m
@@ -23,6 +23,9 @@
 }
 
 - (void)testExample {
+    
+    XCTAssertEqual(1,1,@"111");
+    
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
