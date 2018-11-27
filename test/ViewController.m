ea6ee3d8cb7e93f12b98401f83258f8792f8353e
肖朝阳 <534600451@qq.com>
2018-11-27 13:49:48 +0800

    1

--- test/ViewController.m
+++ test/ViewController.m
@@ -18,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     RequestData *data = [[RequestData alloc] initWithParameter:[PlayParameter new]];
+    NSLog(@"2222");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
